069ec7125b2db4ac6d0b70431438d31fb7b03d11
Sergei US <ustimenko.sv@gmail.com>
2022-09-09 13:47:34 +0300

    Initial commit

--- src/test/java/ru/netology/junit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/junit/CashbackHackServiceTest.java
@@ -11,9 +11,45 @@ class CashbackHackServiceTest {
     void calculateRemain() {
 
         CashbackHackService service = new CashbackHackService();
-        int amount = 1900;
+        int amount = 800;
 
-        int expected = 100;
+        int expected = 200;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateRemainIfAmountZero() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateRemainIfAmountOne() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void calculateRemainIfAmountMoreThenThousand() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1010;
+
+        int expected = 990;
         int actual = service.remain(amount);
 
         Assertions.assertEquals(expected, actual);
